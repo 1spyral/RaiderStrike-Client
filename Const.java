47de167b78cc08cd384d04e84f3ed16fc99bf1d8
1spyral <83098914+1spyral@users.noreply.github.com>
2023-01-06 16:30:43 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -1,6 +1,6 @@
 public class Const {
-    public static final String HOST = "";
-    public static final int PORT = 5001;
+    public static final String HOST = "localhost";
+    public static final int PORT = 135;
 
     public static final int WIDTH = 1800;
     public static final int HEIGHT = 900;
