0c44a46eaaf6910546274599cc7455a03df23ff1
spyral <116putin@gmail.com>
2023-01-11 16:03:32 -0500

    Update MenuState.java

--- MenuState.java
+++ MenuState.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
@@ -8,8 +9,9 @@ public class MenuState extends State {
 
     int id;
     Player[] players;
-    int attack;
-    int defense;
+    int red;
+    int blue;
+    boolean caret;
     String name;
     boolean nameSet;
 
@@ -26,13 +28,23 @@ public class MenuState extends State {
 
         this.id = -1;
         this.players = new Player[6];
-        this.attack = 0;
-        this.defense = 0;
+        this.red = 0;
+        this.blue = 0;
+        this.caret = false;
         this.name = "";
         this.nameSet = false;
     }
     public void update() {
+        while (!this.messenger.isEmpty()) {
+            String[] command = this.messenger.poll().split(" ");
+            System.out.println("dpoasdpoasdi");
+            if (command[0].equals("ID")) {
+                System.out.println("hihi hi id");
+                this.id(Arrays.copyOfRange(command, 1, command.length));
+            }
+        }
         if (!this.nameSet) {
+            this.caret = !this.caret;
 
         }
     }
@@ -40,17 +52,21 @@ public class MenuState extends State {
         if (this.nameSet) {
         }
         else {
-            g.drawImage(this.title, (Const.WIDTH - this.title.getWidth()) / 2, (Const.HEIGHT - this.title.getHeight()) / 3, null);
+            g.drawImage(this.title, (Const.WIDTH - this.title.getWidth()) / 2, (Const.HEIGHT - this.title.getHeight()) / 4, null);
             g.setFont(FontLoader.getFont(80));
             if (this.id == -1) {
                 g.drawString("Waiting on server...", (Const.WIDTH - g.getFontMetrics().stringWidth("Waiting on server...")) / 2, (int)(Const.HEIGHT * 0.6));
             }
             else {
-    
+                g.drawRect((int)(Const.WIDTH * 0.3), (int)(Const.HEIGHT * 0.8), (int)(Const.WIDTH * 0.4), (int)(Const.HEIGHT * 0.1));
             }      
         }
     }
     public void close() {
         super.close();
+    }
+
+    public void id(String[] args) {
+        this.id = Integer.valueOf(args[0]);
     }
 }
