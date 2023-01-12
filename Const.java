b93b0941b4d79f52087ea436c4ccdef8aa1235ba
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-01-12 14:26:43 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -2,8 +2,8 @@ public class Const {
     public static final String HOST = "localhost";
     public static final int PORT = 5000;
 
-    public static final int WIDTH = 1800;
-    public static final int HEIGHT = 900;
+    public static final int WIDTH = 1400;
+    public static final int HEIGHT = 800;
     public static final int FRAME_PERIOD = 20;
 
     private Const(){}
