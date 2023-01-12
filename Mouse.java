11c39d503c3e4135bda801633325235293224c51
1spyral <83098914+1spyral@users.noreply.github.com>
2023-01-11 23:44:00 -0500

    Rework mouse

--- Mouse.java
+++ Mouse.java
@@ -1,82 +1,74 @@
+import java.util.*;
 import java.awt.event.*;
 
 public class Mouse implements MouseListener, MouseMotionListener {
     private int x;
     private int y;
-    private boolean rightMouseClicked;
-    private boolean leftMouseClicked;
-    private boolean rightMouseDown;
-    private boolean leftMouseDown;
+    private Queue<Click> clicks;
+    private boolean[] pressed;
     
+    Mouse() {
+        this.x = -1;
+        this.y = -1;
+        this.clicks = new LinkedList<Click>();
+        this.pressed = new boolean[17];
+    }
+
     public int getX() {
         return this.x;
-    }
-    public void setX(int x) {
-        this.x = x;
     }
     public int getY() {
         return this.y;
     }
-    public void setY(int y) {
-        this.y = y;
+    public Click poll() {
+        return this.clicks.poll();
     }
-    public boolean rightClicked() {
-        return this.rightMouseClicked;
+    public boolean isPressed(int button) {
+        try {
+            return this.pressed[button];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-    protected void rightMouseClick() {
-        this.rightMouseClicked = true;
+
+    public class Click {
+        private int button;
+        private int x;
+        private int y;
+
+        Click(int button, int x, int y) {
+            this.button = button;
+            this.x = x;
+            this.y = y;
+        }
+        
+        public int getButton() {
+            return this.button;
+        }
+        public int getX() {
+            return this.x;
+        }
+        public int getY() {
+            return this.y;
+        }
     }
-    public void rightMouseUnclick() {
-        this.rightMouseClicked = false;
-    }
-    public boolean leftClicked() {
-        return this.leftMouseClicked;
-    }
-    protected void leftMouseClick() {
-        this.leftMouseClicked = true;
-    }
-    public void leftMouseUnclick() {
-        this.leftMouseClicked = false;
-    }
-    public boolean rightDown() {
-        return this.rightMouseDown;
-    }
-    protected void rightMousePressed() {
-        this.rightMouseDown = true;
-    }
-    protected void rightMouseReleased() {
-        this.rightMouseDown = false;
-    }
-    public boolean leftDown() {
-        return this.leftMouseDown;
-    }
-    protected void leftMousePressed() {
-        this.leftMouseDown = true;
-    }
-    protected void leftMouseReleased() {
-        this.leftMouseDown = false;
-    }
+
     /* Inherited from MouseListener */
     public void mouseClicked(MouseEvent e) {
-        if (e.getButton() == 1) {
-            this.rightMouseClick();
-        } else if (e.getButton() == 2) {
-            this.leftMouseClick();
-        }
+        this.clicks.add(new Click(e.getButton(), e.getX(), e.getY()));
     }
     public void mousePressed(MouseEvent e) {
-        if (e.getButton() == 1) {
-            this.rightMousePressed();
-        } else if (e.getButton() == 2) {
-            this.leftMousePressed();
-        }
+        try {
+            this.pressed[e.getButton()] = true;
+        } catch (Exception error) {}
     }
     public void mouseReleased(MouseEvent e) {
-        if (e.getButton() == 1) {
-            this.rightMouseReleased();
-        } else if (e.getButton() == 2) {
-            this.leftMouseReleased();
-        }
+        try {
+            this.pressed[e.getButton()] = false;
+        } catch (Exception error) {}
     }
     public void mouseEntered(MouseEvent e) {
     }
@@ -84,11 +76,12 @@ public class Mouse implements MouseListener, MouseMotionListener {
     }
     /* Inherited from MouseMotionListener */
     public void mouseMoved(MouseEvent e) {
-        this.setX(e.getX());
-        this.setY(e.getY());
+        this.x = e.getX();
+        this.y = e.getY();
     }
     public void mouseDragged(MouseEvent e) {
-        this.setX(e.getX());
-        this.setY(e.getY());
-    }         
+        this.x = e.getX();
+        this.y = e.getY();
+    }
+
 }
