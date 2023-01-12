7e8ee22976d1b870df2a8d5be50355001a389cc2
spyral <116putin@gmail.com>
2023-01-12 16:38:25 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -1,10 +1,12 @@
 public class Const {
     public static final String HOST = "localhost";
-    public static final int PORT = 5000;
+    public static final int PORT = 5001;
 
     public static final int WIDTH = 1400;
     public static final int HEIGHT = 800;
     public static final int FRAME_PERIOD = 20;
+
+    public static final int MAX_NAME_LENGTH = 24;
 
     private Const(){}
 }
