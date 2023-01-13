bdcd854c3a710b4c98c341fd31bd89e252afd37f
1spyral <83098914+1spyral@users.noreply.github.com>
2023-01-13 17:51:07 -0500

    Update font/text support

--- FontLoader.java
+++ FontLoader.java
@@ -4,7 +4,8 @@ import java.awt.*;
 // Class for loading and storing fonts
 public class FontLoader {
     private static final String FONT_PATH = "assets/ComicMono.ttf";
-    private static Font FONT = loadFont();
+    
+    private static Font font = loadFont();
     private static Font[] fonts = new Font[100];
     private static Font loadFont() {
         try {
@@ -17,7 +18,7 @@ public class FontLoader {
     // Each font size only needs to be loaded once
     public static Font getFont(int size) {
         if (fonts[size - 1] == null) {
-            fonts[size - 1] = FONT.deriveFont((float)size);
+            fonts[size - 1] = font.deriveFont((float)size);
         }
         return fonts[size - 1];
     }
---
+++ Text.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class Text {
+    private Text() {}
+
+    // Draw text from x centered at y
+    public static void draw(Graphics g, int fontSize, String text, double x, double y) {
+        g.setFont(FontLoader.getFont(fontSize));
+        FontMetrics fm = g.getFontMetrics();
+        g.drawString(text, (int)x, (int)y - fm.getHeight() / 2 + fm.getAscent());
+    }
+    // Draw text centered at (x, y)
+    public static void drawCentered(Graphics g, int fontSize, String text, double x, double y) {
+        g.setFont(FontLoader.getFont(fontSize));
+        FontMetrics fm = g.getFontMetrics();
+        g.drawString(text, (int)x - fm.stringWidth(text) / 2, (int)y - fm.getHeight() / 2 + fm.getAscent());
+    }
+    // Draw text centered at the provided rectangle
+    public static void drawCentered(Graphics g, int fontSize, String text, double x, double y, int width, int height) {
+        drawCentered(g, fontSize, text, x + width / 2, y + height / 2);
+    }
+    // Draw text centered at the provided rectangle
+    public static void drawCentered(Graphics g, int fontSize, String text, Rectangle rect) {
+        drawCentered(g, fontSize, text, (int)rect.getCenterX(), (int)rect.getCenterY());
+    }
+}
