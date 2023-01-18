ffb0c410949887cb4304ca207a92a50a2883ab20
1spyral <83098914+1spyral@users.noreply.github.com>
2023-01-18 00:04:47 -0500

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -1,28 +1,31 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
 public enum Agent {
-    ZERO(0, 1, 0),
-    MUSTANG(1, 2, 0),
-    HELLCAT(2, 3, 0),
-    WILDCAT(3, 4, 0),
-    MIRAGE(4, 5, 0);
+    ZERO(0, "assets/zero.png"),
+    MUSTANG(1, "assets/mustang.png"),
+    HELLCAT(2, ""),
+    WILDCAT(3, ""),
+    MIRAGE(4, "");
 
 //-------------------------------------------------------------------------------------
-    private final int agentId;
-    private final int basicAbility; // ability Id
-    private final int ultimateAbility; // ability Id
+    private final int id;
+    private BufferedImage icon;
 
-    Agent(int agentId, int basicAbility, int ultimateAbility) {
-        this.agentId = agentId;
-        this.basicAbility = basicAbility;
-        this.ultimateAbility = ultimateAbility;
+    Agent(int id, String iconPath) {
+        this.id = id;
+        try {
+            this.icon = ImageIO.read(new File(iconPath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    public int getAgentId(){
-        return this.agentId;
+    public int getId(){
+        return this.id;
     }
-    public int getBasicAbility(){
-        return this.basicAbility;
-    }
-    public int getUltimateAbility(){
-        return this.ultimateAbility;
+    public Image getIcon() {
+        return this.icon;
     }
 }
